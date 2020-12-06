822d669b100ec9c373aff53ba1b42bd24b450a28
seiftahawy54 <seiftahawy54@gmail.com>
2020-12-06 14:15:59 +0200

    App changed to functional, Async Component replaced

--- src/App.js
+++ src/App.js
@@ -1,79 +1,86 @@
 // React Main Library && File important imports
-import React, { Component } from 'react';
+import React, { useEffect, Suspense } from "react";
 // Importing react router to implement this feature.
-import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 // Importing connect to add the redux's state management.
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 // Components
-import Layout from './hoc/Layout/Layout';
+import Layout from "./hoc/Layout/Layout";
 
 // Containers
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Logout from './containers/Auth/Logout/Logout';
-// Importing AsyncComponent For lazy loading.
-import AsyncComponent from './hoc/AsyncComponent/AsyncComponent';
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Logout from "./containers/Auth/Logout/Logout";
 // Importing all actions from the index file.
-import * as actions from './store/actions/index';
+import * as actions from "./store/actions/index";
 
-const asyncCheckout = AsyncComponent(() => {
-  return import('./containers/Checkout/Checkout');
+// Implementing lazy loading functionality.
+const Checkout = React.lazy(() => {
+  return import("./containers/Checkout/Checkout");
 });
-const asyncAuth = AsyncComponent(() => {
-  return import('./containers/Auth/Auth');
+const Auth = React.lazy(() => {
+  return import("./containers/Auth/Auth");
 });
-const asyncOrders = AsyncComponent(() => {
-  return import('./containers/Orders/Orders');
+const Orders = React.lazy(() => {
+  return import("./containers/Orders/Orders");
 });
 
-class App extends Component {
+const App = (props) => {
+  useEffect(() => {
+    props.onTryAutoSignUp();
+  }, []);
 
-  componentDidMount() {
-    this.props.onTryAutoSignUp();
-  }
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={() => <Auth />} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Route
+        render={() => (
+          <h1 style={{ textAlign: "center", color: "red" }}>PAGE NOT FOUND!</h1>
+        )}
+      />
+      <Redirect to="/" />
+    </Switch>
+  );
 
-  render () {
-    let routes = (
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path="/auth" component={asyncAuth} />
-        <Route path="/" exact component={BurgerBuilder}/>
-        <Route render={() => <h1 style={{textAlign: "center", color:"red"}}>PAGE NOT FOUND!</h1>}/>
+        <Route path="/orders" render={() => <Orders />} />
+        <Route path="/checkout" render={() => <Checkout />} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" render={() => <Auth />} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route
+          render={() => (
+            <h1 style={{ textAlign: "center", color: "red" }}>
+              PAGE NOT FOUND!
+            </h1>
+          )}
+        />
         <Redirect to="/" />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/orders" component={asyncOrders}/>
-          <Route path="/checkout" component={asyncCheckout} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={asyncAuth} />
-          <Route path="/" exact component={BurgerBuilder}/>
-          <Route render={() => <h1 style={{textAlign: "center", color:"red"}}>PAGE NOT FOUND!</h1>}/>
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-    return (
-      <div>
-        <Layout>
-          {routes}
-        </Layout>
-      </div>
-    )
   }
-}
 
-const mapStateToProps = state => {
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </Layout>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
   return {
-    isAuthenticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    onTryAutoSignUp: () => dispatch(actions.authCheckState())
+    onTryAutoSignUp: () => dispatch(actions.authCheckState()),
   };
 };
 
